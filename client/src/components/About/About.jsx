2881f4d7d20270977e6a510bd1d36539bd7250f6
Long Nguyen <longnp1610@gmail.com>
2022-09-06 17:17:46 +0700

    Responsive for Showrooms Section

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -31,19 +31,21 @@ const About = () => {
   }, []);
   return (
     <div className="overflow-x-hidden mt-16">
-      <div className="flex">
+      <div className="lg:flex lg:flex-row flex-col">
         <div
           data-aos="fade-right"
-          className="w-1/2 flex flex-col bg-[#0F1520] text-white justify-center pl-20"
+          className="lg:w-1/2 w-full flex flex-col bg-[#0F1520] text-white justify-center lg:px-14 p-14"
         >
-          <h2 className="uppercase w-fit font-bold text-4xl">
+          <h2 className="uppercase w-fit font-bold xl:text-4xl text-2xl">
             {descriptions.showrooms.title}
           </h2>
-          <h4 className="uppercase w-fit font-semibold text-2xl mb-2 ">
+          <h4 className="uppercase w-fit font-semibold xl:text-2xl mb-2 ">
             {descriptions.showrooms.sub}
           </h4>
-          <span className="mb-5 w-fit">{descriptions.showrooms.caption}</span>
-          <button className="border p-[11px_18px] font-semibold border-white w-fit uppercase">
+          <span className="mb-5 w-fit xl:text-base text-sm">
+            {descriptions.showrooms.caption}
+          </span>
+          <button className="border p-[11px_18px] font-semibold border-white w-fit uppercase xl:text-base text-sm">
             <Link to="#" className="flex">
               Find showrooms in your place
               <ArrowRightIcon
@@ -53,7 +55,7 @@ const About = () => {
             </Link>
           </button>
         </div>
-        <div data-aos="fade-left" className="w-1/2">
+        <div data-aos="fade-left" className="lg:w-1/2 w-full">
           <img
             src={descriptions.showrooms.image}
             alt="store_image"
